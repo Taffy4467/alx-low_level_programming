d6d0ea6e9f71c49a92f19e851d5b5173dc1e2b6e
Tafadzwa <gandarit4467@gmail.com>
2022-09-14 07:10:16 -0700

    0. _putchar

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,19 @@
+# include "main.h"
+
+/* main - Prints putchar and returns new line.
+ *
+ * Return always : 0
+ */
+int main(void)
+{
+	_putchar ('p');
+	_putchar ('u');
+	_putchar ('t');
+	_putchar ('c');
+	_putchar ('h');
+	_putchar ('a');
+	_putchar ('r');
+	_putchar ('\n');
+
+	return 0;
+}
