a1f45390d52f41243d9edf117580d3dcac655947
Tafadzwa <gandarit4467@gmail.com>
2022-10-31 13:02:59 -0700

    redoing create file

--- 0x15-file_io/1-create_file.c
+++ 0x15-file_io/1-create_file.c
@@ -8,30 +8,26 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int fd;
+	int file, i = 0;
 
-	if (!filename)
+	if (filename == NULL)
 		return (-1);
 
-	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR);
-	if (fd == -1)
+	file = open(filename, O_CREAT | O_TRUNC | O_WRONLY, 0600);
+	if (file == -1)
 		return (-1);
 
-	if (text_content)
-		write(fd, text_content, _strlen(text_content));
-	close(fd);
+	while (text_content[i])
+		i++;
+	if (text_content == NULL)
+	{
+		close(file);
+		return (-1);
+	}
+	else
+	{
+		write(file, text_content, i);
+	}
+	close(file);
 	return (1);
-}
-/**
- * _strlen - Returns the length of a string
- * @s: String to count
- * Return: String length
- */
-int _strlen(char *s)
-{
-	int c = 0;
-
-	while (s[c])
-		c++;
-	return (c);
 }
