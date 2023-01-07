c5538a28b8f4a19c39e2cdb994533399c37ea7ce
Tafadzwa <gandarit4467@gmail.com>
2023-01-07 02:04:46 -0800

    5,6-hash files

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -11,10 +11,8 @@ void hash_table_print(const hash_table_t *ht)
 	hash_node_t *node;
 	unsigned long int i;
 	unsigned char comma_flag = 0;
-	
 	if (ht == NULL)
 		return;
-	
 	printf("{");
 	for (i = 0; i < ht->size; i++)
 	{
@@ -22,7 +20,6 @@ void hash_table_print(const hash_table_t *ht)
 		{
 			if (comma_flag == 1)
 				printf(", ");
-			
 			node = ht->array[i];
 			while (node != NULL)
 			{
