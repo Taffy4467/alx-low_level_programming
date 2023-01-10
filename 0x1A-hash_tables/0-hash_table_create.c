93fb19d8dd8afeab138a4f89d1494f04f4ad33c9
tafadzwa <gandarit4467@gmail.com>
2023-01-10 05:24:25 -0800

    update Readme

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -1,51 +1,27 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "hash_tables.h"
 
 /**
  * hash_table_create - Creates a hash table.
  * @size: The size of the array.
+ *
  * Return: If an error occurs - NULL.
- * Otherwise - a pointer to the new hash table.
+ *         Otherwise - a pointer to the new hash table.
  */
-typedef struct node
-{
-	char *key;
-	char *value;
-	struct node *next;
-} node_t;
-
-typedef struct hash_table
-{
-	unsigned long int size;
-	struct node **array;
-} hash_table_t;
-
-/**Allocate memory for the hash table*/
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_table_t *hash_table = NULL;
-	if (size < 1)
-	{
+	hash_table_t *ht;
+	unsigned long int i;
+
+	ht = malloc(sizeof(hash_table_t));
+	if (ht == NULL)
 		return (NULL);
-	}
-	hash_table = malloc(sizeof(hash_table_t));
-	if (hash_table == NULL)
-	{
+
+	ht->size = size;
+	ht->array = malloc(sizeof(hash_node_t *) * size);
+	if (ht->array == NULL)
 		return (NULL);
-	}
-/**Allocate memory for the array of nodes*/
-hash_table->array = malloc(sizeof(node_t *) * size);
-if (hash_table->array == NULL)
-{
-	return (NULL);
-}
-for (unsigned long int i = 0; i < size; i++)
-{
-	hash_table->array[i] = NULL;
-}
-/**Set the size of the hash table*/
-hash_table->size = size
-return (hash_table);
+	for (i = 0; i < size; i++)
+		ht->array[i] = NULL;
+
+	return (ht);
 }
